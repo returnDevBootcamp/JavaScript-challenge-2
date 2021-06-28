f4b54a320e620415d9c5e48dd036d82373b27521
maxamed ibraahim <83146803+moshibbin252@users.noreply.github.com>
2021-06-28 06:48:49 +0300

    challange of home work

--- Part-2/challenge.js
+++ Part-2/challenge.js
@@ -1,2 +1,17 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+var red=(23+45+56);
+var blue =(34+54+23);
+
+var avgReds =red;
+var avgBlues= blue;
+var scormain =100;
+if(avgReds&&avgBlues>scormain){
+    console.log(" red is win")
+}
+else if (avgReds&&avgBlues<scormain){
+    console.log("blue is win")
+}
+else{
+    console.log("draw")
+}
