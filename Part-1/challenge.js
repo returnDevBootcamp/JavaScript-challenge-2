f682cc1ad94eadec249bce2b283615cd14437295
Sara-Issa <85680247+Sara-Issa@users.noreply.github.com>
2021-06-29 19:32:01 +0300

    done

--- Part-1/challenge.js
+++ Part-1/challenge.js
@@ -1,2 +1,19 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+var red = 96 + 108 + 89;
+var blue = 88 + 91 + 110;
+
+console.log(red);
+console.log(blue);
+
+var redavr = red / 3;
+var blueavr = blue / 3;
+
+console.log(redavr);
+console.log(blueavr);
+
+if (redavr > blueavr) {
+  console.log("red won");
+} else if (redavr < blueavr) {
+  console.log("blue won");
+} else {
+  console.log("both won");
+}
